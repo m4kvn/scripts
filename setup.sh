94e0be5f30dca441f82e221af8f5023cbd72126b
齊藤正宏 <fun.masahiro@gmail.com>
2018-02-28 16:07:49 +0900

    update setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 dir=$(cd $(dirname $0) && pwd)
-cat << EOF >> ~/.bash_profile
-
+cat << EOF > profile
 export PATH=${dir}/android:\$PATH
+EOF
+
+cat << EOF >> ~/.bash_profile
+# Auto setting by github.com/m4kvn/scripts
+if [ -s $dir ]; then
+    source ${dir}/profile
+fi
 EOF
